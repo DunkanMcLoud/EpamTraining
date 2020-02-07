87ab3ab48fda68cf60403f3987781e0e6546e122
Andrei Dunai <48240257+DunkanMcLoud@users.noreply.github.com>
2020-02-07 17:22:49 +0300

    Add files via upload

--- homework24/application/Application.java
+++ homework24/application/Application.java
@@ -1,26 +1,16 @@
 package ru.epam.javacore.lesson_18_19_20_java_8.homework.application;
 
 import ru.epam.javacore.lesson_18_19_20_java_8.homework.cargo.repo.impl.CargoDatabaseRepoImplementation;
-import ru.epam.javacore.lesson_18_19_20_java_8.homework.cargo.service.CargoDatabaseServiceImpl;
 import ru.epam.javacore.lesson_18_19_20_java_8.homework.cargo.service.CargoService;
-import ru.epam.javacore.lesson_18_19_20_java_8.homework.common.business.exception.checked.InitStorageException;
-import ru.epam.javacore.lesson_18_19_20_java_8.homework.storage.initor.InitStorageType;
-import ru.epam.javacore.lesson_18_19_20_java_8.homework.storage.initor.StorageInitor;
-
-import static ru.epam.javacore.lesson_18_19_20_java_8.homework.storage.initor.StorageInitorFactory.getStorageInitor;
+import ru.epam.javacore.lesson_18_19_20_java_8.homework.cargo.service.CargoServiceImpl;
 
 public class Application {
 
     private static final String SEPARATOR = "--------------";
-    private static CargoService cargoService = new CargoDatabaseServiceImpl(new CargoDatabaseRepoImplementation());
+    private static CargoService cargoService = new CargoServiceImpl(new CargoDatabaseRepoImplementation());
 
     public static void main(String[] args) {
-        StorageInitor storageInitor = getStorageInitor(InitStorageType.SQL);
-        try {
-            storageInitor.initStorage();
-        } catch (InitStorageException e) {
-            e.printStackTrace();
-        }
+        cargoService.printAll();
     }
 }
 
